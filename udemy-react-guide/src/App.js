8c61293cd6578a494eca095a4a139a68ed3430d3
Artem Bayandin <bayandin.artem.official@gmail.com>
2020-04-26 18:50:20 +0300

    styles added

--- udemy-react-guide/src/App.js
+++ udemy-react-guide/src/App.js
@@ -3,9 +3,17 @@ import './App.css';
 import Person from './Person/Person'
 
 const App = props => {
-    const [ peopleState, setPeople ] = useState({
-        people: [ { id: 1, name: 'Bob', age: 18 }, { id: 2, name: 'John', age: 33 }, { id: 3, name: 'Mary', age: 25 }]
-    })
+    const initialState = {
+        people: [
+            { id: 1, name: 'Bob', age: 18 }, 
+            { id: 2, name: 'John', age: 33 },  
+            { id: 3, name: 'Daddy', age: 65 },  
+            { id: 4, name: 'Diana', age: 21 }, 
+            { id: 5, name: 'Mary', age: 25 }
+        ]
+    }
+
+    const [ peopleState, setPeople ] = useState(initialState)
 
     const addAgeToAll = () => {
         setPeople(prevState => { 
@@ -27,18 +35,37 @@ const App = props => {
         })
     }
 
+    const deletePersonById = (id) => {
+        setPeople(prevState => { 
+            return { people: [...prevState.people.filter(p => p.id !== id)] }
+        })
+    }
+
     const [ viewPeople, setViewPeople ] = useState(false)
 
     const buttonStyle = {
         backgroundColor: 'white',
         border: '2px solid #aaa',
         padding: '5px 10px',
-        fontSize: '16px'
+        fontSize: '16px',
+        marginRight: '10px'
+    }
+
+    const resetBtnStyles = [];
+    if (peopleState.people.length > 3) {
+        resetBtnStyles.push('combined-italic')
+    }
+    if (peopleState.people.length < 3) {
+        resetBtnStyles.push('combined-bold')
+    }
+    if (peopleState.people.length < 1) {
+        resetBtnStyles.push('combined-red')
     }
 
     return (
         <div className="App">
             <h1>hello</h1>
+            <button onClick={() => setPeople(initialState)} style={buttonStyle} className={resetBtnStyles.join(' ')}>reset</button>
             <button onClick={addAgeToAll} style={buttonStyle}>add age to everybody</button>
             <button onClick={() => setViewPeople(prevState => !prevState)} style={buttonStyle}>toggle view</button>
             {
@@ -51,6 +78,7 @@ const App = props => {
                         age={item.age}
                         makeOlder={(ev) => makeOneOlderHandler(ev, item.id)} 
                         nameChanged={(ev) => nameChangedHandler(ev, item.id)}
+                        click={() => deletePersonById(item.id)}
                     />)
             }
         </div>
--- udemy-react-guide/src/Person/Person.js
+++ udemy-react-guide/src/Person/Person.js
@@ -2,12 +2,23 @@ import React from 'react'
 import './Person.css'
 
 const person = (props) => {
+    const addAgeClickHandler = (ev) => {
+        ev.stopPropagation()
+        props.makeOlder(ev)
+    }
+
     return (
         <div className="Person">
             <p>My name is {props.name} and I'm {props.age} years old</p>
             <p>{props.children}</p>
-            <button onClick={props.makeOlder}>add age to {props.name}</button>
-            <input type="text" onChange={props.nameChanged} value={props.name} />
+            <p>
+                <input type="text" onChange={props.nameChanged} value={props.name} />
+            </p>
+            <div>
+                <button onClick={(ev) => addAgeClickHandler(ev)}>add age to {props.name}</button>
+                <button onClick={props.click}>delete</button>
+            </div>
+            
         </div>
     )
 }
