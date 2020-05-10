9d4537c76b424958a1185860c5d7258413c7fbf0
Artem Bayandin <bayandin.artem.official@gmail.com>
2020-05-10 13:45:07 +0300

    react-transition-group added with test sample

--- animations/src/App.js
+++ animations/src/App.js
@@ -5,19 +5,46 @@ import Modal from "./components/Modal/Modal"
 import Backdrop from "./components/Backdrop/Backdrop"
 import List from "./components/List/List"
 
+import Transition from 'react-transition-group/Transition'
+
 class App extends Component {
     state = {
-        modalIsOpen: false
+        modalIsOpen: false,
+        showBlock: false
     }
 
     showModal = () => this.setState({modalIsOpen: true})
 
     closeModal = () => this.setState({modalIsOpen: false})
 
+    toggleBlock = () => this.setState(prevState => { return { showBlock: !prevState.showBlock } })
+
     render() {
         return (
             <div className="App">
                 <h1>React Animations</h1>
+
+                <div style={{marginBottom: '20px'}}>
+                    <button className="Button" onClick={this.toggleBlock}>toggle</button>
+                    <Transition
+                        in={this.state.showBlock}
+                        timeout={1000}
+                        mountOnEnter
+                        unmountOnExit
+                    >
+                        {state => (
+                            <div style={{
+                                backgroundColor: 'red',
+                                padding: '10px 20px',
+                                color: 'white',
+
+                                transition: 'opacity 1s ease-out',
+                                opacity: state === 'exiting' ? 0 : 1
+                            }}>block</div>
+                        )}
+                    </Transition>
+                </div>
+
                 <Modal
                     show={this.state.modalIsOpen}
                     closed={this.closeModal}
