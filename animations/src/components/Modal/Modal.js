9aba758f477fe7d72fcbc6a2160f1fdfad4fd414
Artem Bayandin <bayandin.artem.official@gmail.com>
2020-05-10 14:07:02 +0300

    CSS transitions

--- animations/src/components/Modal/Modal.js
+++ animations/src/components/Modal/Modal.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import Transition from 'react-transition-group/Transition'
+import CSSTransition from 'react-transition-group/CSSTransition'
 
 import './Modal.css'
 
@@ -11,7 +12,38 @@ const animationTiming = {
 
 const modal = (props) => {
     return (
-        <Transition
+        // <Transition
+        //     in={props.show}
+        //     timeout={animationTiming}
+        //     mountOnEnter
+        //     unmountOnExit
+        //     onEnter={() => console.log('[Modal] 1.onEnter')}
+        //     onEntering={() => console.log('[Modal] 2.onEntering')}
+        //     onEntered={() => console.log('[Modal] 3.onEntered')}
+        //     onExit={() => console.log('[Modal] 4.onExit')}
+        //     onExiting={() => console.log('[Modal] 5.onExiting')}
+        //     onExited={() => console.log('[Modal] 6.onExited')}
+        // >
+        //     {state => {
+        //         const cssClasses = [
+        //             'Modal', 
+        //             state === 'entering'
+        //                 ? 'ModalOpen'
+        //                 : state === 'exiting'
+        //                     ? 'ModalClosed'
+        //                     : null
+        //         ]
+
+        //         return (
+        //             <div className={cssClasses.join(' ')}>
+        //                 <h1>A Modal</h1>
+        //                 <button className="Button" onClick={props.closed}>Dismiss</button>
+        //             </div>
+        //         )
+        //     }}
+        // </Transition>
+
+        <CSSTransition
             in={props.show}
             timeout={animationTiming}
             mountOnEnter
@@ -22,25 +54,13 @@ const modal = (props) => {
             onExit={() => console.log('[Modal] 4.onExit')}
             onExiting={() => console.log('[Modal] 5.onExiting')}
             onExited={() => console.log('[Modal] 6.onExited')}
+            classNames="Modal-Fade"
         >
-            {state => {
-                const cssClasses = [
-                    'Modal', 
-                    state === 'entering'
-                        ? 'ModalOpen'
-                        : state === 'exiting'
-                            ? 'ModalClosed'
-                            : null
-                ]
-
-                return (
-                    <div className={cssClasses.join(' ')}>
-                        <h1>A Modal</h1>
-                        <button className="Button" onClick={props.closed}>Dismiss</button>
-                    </div>
-                )
-            }}
-        </Transition>
+            <div className='Modal'>
+                <h1>A Modal</h1>
+                <button className="Button" onClick={props.closed}>Dismiss</button>
+            </div>
+        </CSSTransition>
     )
 }
 
